f2ec924dc613424026ba3662b48fbc51d914533c
huaxiaobin <931452565@qq.com>
2018-05-07 14:43:50 +0800

    初始化仓库

--- app/src/main/java/com/yhslib/lottery/fragment/ResultFragment.java
+++ app/src/main/java/com/yhslib/lottery/fragment/ResultFragment.java
@@ -3,7 +3,6 @@ package com.yhslib.lottery.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ public class ResultFragment extends Fragment {
         Bundle args = new Bundle();
         ResultFragment fragment = new ResultFragment();
         fragment.setArguments(args);
-
         return fragment;
     }
 
